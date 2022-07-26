6791faf06a54b04d92a2bd67735aa797c194e135
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2022-07-26 00:39:51 -0700

    Collections routing
    routes\collections.js
    - Removed 'validateCampground' and 'isAuthor' destructured requires
    
    BUGS
    views\home.ejs
    - Path for 'My Collections' button changed to '/collections'
         (Need to check routing to ensure 'index.ejs' filename is cleansed

--- routes/collections.js
+++ routes/collections.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
 const catchAsync = require("../utilities/catchAsync");
-const { isLoggedIn, validateCampground, isAuthor } = require("../middleware");
+const { isLoggedIn } = require("../middleware");
 const multer = require("multer");
 const { storage } = require("../cloudinary");
 const upload = multer({ storage });
